0305b815d6922906a34ede0765704c772562f011
Michael Kopchenov <michaelkopchenov@yandex.ru>
2023-12-11 09:12:35 +0300

    refactor: Version 2.1 (create slider)

--- src/components/Desc/Desc.jsx
+++ src/components/Desc/Desc.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React from 'react';
+import { React } from 'react';
 import Slider from '../Slider/Slider';
 import Laptop from '../../images/laptop.png';
 import Time from '../../images/time.png';
@@ -12,17 +12,12 @@ import GirlStrong from '../../images/girl-strong.svg';
 import GirlPan from '../../images/girl-pan.svg';
 import GirlCont from '../../images/girl-cont.svg';
 import Cloud from '../../images/cloud.svg'
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import './Desc.css';
 
 function Desc() {
     return (
         <Slider>
             <section className='desc'>
-                {/* <button className='desc__btn_prev desc__button_slider'></button>
-                <button className='desc__btn_next desc__button_slider'></button> */}
                 <div className='desc__block'>
                     <h2 className='desc__title'>
                         Разнообразим твой образовательный процесс
@@ -178,247 +173,42 @@ function Desc() {
                     />
                 </div>
             </section>
+            <section className='desc__last'>
+                <h1>ХОЧЕШЬ УЗНАТЬ БОЛЬШЕ?</h1>
+                <div className='desc__last-block'>
+                <div className='desc__block'>
+                    <h2 className='desc__title'>
+                        ТЫ студент или только планируешь поступление в вуз?
+                    </h2>
+                    <div className='desc__container'>
+                        <p className='desc__text'>
+                            Оставляй заявку на участие в проекте и мы обязательно свяжемся с тобой. Твоя обратная связь поможет нам сделать наш продукт еще лучше!
+                        </p>
+                        <img 
+                            src={Boy} 
+                            alt="Иконка"  
+                            className='desc__image'
+                        />
+                    </div>
+                </div>
+                <div className='desc__block'>
+                    <h2 className='desc__title'>
+                        ТЫ представитель образовательной организации?
+                    </h2>
+                    <div className='desc__container'>
+                        <p className='desc__text'>
+                            Мы ищем сотрудничества с ВУЗами по всей России, готовы обмениваться идеями и вместе развивать технологии индивидуализации обучения
+                        </p>
+                        <img 
+                            src={Time} 
+                            alt="Иконка"  
+                            className='desc__image'
+                        />
+                    </div>
+                </div>
+                </div>
+            </section>
         </Slider>
-        // <>
-        //     <div>
-        //         <div class="carousel">
-        //             <ul class="slides">
-        //             <input type="radio" name="radio-buttons" id="img-1" checked />
-        //             <li class="slide-container">
-        //                 <div class="slide-image">
-        //                 <section className='desc' id='slider'>
-        //                         <div className='desc__block'>
-        //                             <h2 className='desc__title'>
-        //                                 Разнообразим твой образовательный процесс
-        //                             </h2>
-        //                             <div className='desc__container'>
-        //                                 <p className='desc__text'>
-        //                                     Предоставим возможность выбора профессии мечты с учетом программы бакалавриата и желания развиваться
-        //                                 </p>
-        //                                 <img 
-        //                                     src={Laptop} 
-        //                                     alt="Иконка"  
-        //                                     className='desc__image'
-        //                                 />
-        //                             </div>
-        //                         </div>
-        //                         <div className='desc__block'>
-        //                             <h2 className='desc__title'>
-        //                                 Дадим экспертные рекомендации
-        //                             </h2>
-        //                             <div className='desc__container'>
-        //                                 <p className='desc__text'>
-        //                                     Подскажем пути достижения цели по выбранному направлению, порекомендуем каналы, подкасты, исследования, статьи и книги
-        //                                 </p>
-        //                                 <img 
-        //                                     src={Time} 
-        //                                     alt="Иконка"  
-        //                                     className='desc__image'
-        //                                 />
-        //                             </div>
-        //                         </div>
-        //                         <div className='desc__block'>
-        //                             <h2 className='desc__title'>
-        //                                 Сформируем актуальное резюме
-        //                             </h2>
-        //                             <div className='desc__container'>
-        //                                 <p className='desc__text'>
-        //                                     Создадим резюме, которое будет наполняться автоматически по ходу обучения в Академии и с использованием внешнего контента
-        //                                 </p>
-        //                                 <img 
-        //                                     src={Resume} 
-        //                                     alt="Иконка"  
-        //                                     className='desc__image'
-        //                                 />
-        //                             </div>
-        //                         </div>
-        //                         <div className='desc__block'>
-        //                             <h2 className='desc__title'>
-        //                                 Поможем устроиться на работу мечты
-        //                             </h2>
-        //                             <div className='desc__container'>
-        //                                 <p className='desc__text'>
-        //                                     Предоставим дополнительную информацию о работодателях, поделимся релевантными вакансиями и контактами партнеров Академии
-        //                                 </p>
-        //                                 <img 
-        //                                     src={Boy} 
-        //                                     alt="Иконка"  
-        //                                     className='desc__image'
-        //                                 />
-        //                             </div>
-        //                         </div>
-        //                     </section>
-        //                 </div>
-        //                 <div class="carousel-controls">
-        //                 <label for="img-3" class="prev-slide">
-        //                     <span>&lsaquo;</span>
-        //                 </label>
-        //                 <label for="img-2" class="next-slide">
-        //                     <span>&rsaquo;</span>
-        //                 </label>
-        //                 </div>
-        //             </li>
-        //             <input type="radio" name="radio-buttons" id="img-2" />
-        //             <li class="slide-container">
-        //                 <div class="slide-image">
-        //                 <section className='desc__block2' id='slider'>
-        //                      <img src={GirlThink} alt="Girl" />
-        //                      <div className='desc__block2_container'>
-        //                          <h2 className='desc__title_type'>
-        //                              НЕ ЗНАЕШЬ КЕМ СТАНЕШЬ ПОСЛЕ ОКОНЧАНИЯ ВУЗА?
-        //                          </h2>
-        //                          <p className='desc__text_type'>
-        //                              Не можешь определиться с будущей профессией, но есть желание стать востребованным и успешным специалистом?
-        //                          </p>
-        //                      </div>
-        //                      <div className='desc__block2_three'>
-        //                          <img 
-        //                              src={PlayBtn} 
-        //                              alt="Button" 
-        //                              className='desc__button'
-        //                          />
-        //                      </div>
-        //                  </section>
-        //                 </div>
-        //                 <div class="carousel-controls">
-        //                 <label for="img-1" class="prev-slide">
-        //                     <span>&lsaquo;</span>
-        //                 </label>
-        //                 <label for="img-3" class="next-slide">
-        //                     <span>&rsaquo;</span>
-        //                 </label>
-        //                 </div>
-        //             </li>
-        //             <input type="radio" name="radio-buttons" id="img-3" />
-        //             <li class="slide-container">
-        //                 <div class="slide-image">
-        //                 <section className='desc__block2' id='slider'>
-        //                      <img src={GirlTable} alt="Girl" />
-        //                      <div className='desc__block2_container'>
-        //                          <h2 className='desc__title_type'>
-        //                              Пройди тестирование
-        //                          </h2>
-        //                          <p className='desc__text_type'>
-        //                              Мы поможем тебе определить несколько подходящих профессий, исходя из твоих интересов
-        //                          </p>
-        //                      </div>
-        //                      <div className='desc__block2_three desc__block2_three_three'>
-        //                          <img 
-        //                              src={PlayBtn} 
-        //                              alt="Button" 
-        //                              className='desc__button'
-        //                          />
-        //                      </div>
-        //                  </section>
-        //                 </div>
-        //                 <div class="carousel-controls">
-        //                 <label for="img-2" class="prev-slide">
-        //                     <span>&lsaquo;</span>
-        //                 </label>
-        //                 <label for="img-1" class="next-slide">
-        //                     <span>&rsaquo;</span>
-        //                 </label>
-        //                 </div>
-        //             </li>
-        //             <li class="slide-container">
-        //                 <div class="slide-image">
-        //                 <section className='desc__block2' id='slider'>
-        //                      <img src={GirlTable} alt="Girl" />
-        //                      <div className='desc__block2_container'>
-        //                          <h2 className='desc__title_type'>
-        //                              Пройди тестирование
-        //                          </h2>
-        //                          <p className='desc__text_type'>
-        //                              Мы поможем тебе определить несколько подходящих профессий, исходя из твоих интересов
-        //                          </p>
-        //                      </div>
-        //                      <div className='desc__block2_three desc__block2_three_three'>
-        //                          <img 
-        //                              src={PlayBtn} 
-        //                              alt="Button" 
-        //                              className='desc__button'
-        //                          />
-        //                      </div>
-        //                  </section>
-        //                 </div>
-        //                 <div class="carousel-controls">
-        //                 <label for="img-2" class="prev-slide">
-        //                     <span>&lsaquo;</span>
-        //                 </label>
-        //                 <label for="img-1" class="next-slide">
-        //                     <span>&rsaquo;</span>
-        //                 </label>
-        //                 </div>
-        //             </li>
-        //             <li class="slide-container">
-        //                 <div class="slide-image">
-        //                 <section className='desc__block2' id='slider'>
-        //                      <img src={GirlTable} alt="Girl" />
-        //                      <div className='desc__block2_container'>
-        //                          <h2 className='desc__title_type'>
-        //                              Пройди тестирование
-        //                          </h2>
-        //                          <p className='desc__text_type'>
-        //                              Мы поможем тебе определить несколько подходящих профессий, исходя из твоих интересов
-        //                          </p>
-        //                      </div>
-        //                      <div className='desc__block2_three desc__block2_three_three'>
-        //                          <img 
-        //                              src={PlayBtn} 
-        //                              alt="Button" 
-        //                              className='desc__button'
-        //                          />
-        //                      </div>
-        //                  </section>
-        //                 </div>
-        //                 <div class="carousel-controls">
-        //                 <label for="img-2" class="prev-slide">
-        //                     <span>&lsaquo;</span>
-        //                 </label>
-        //                 <label for="img-1" class="next-slide">
-        //                     <span>&rsaquo;</span>
-        //                 </label>
-        //                 </div>
-        //             </li>
-        //             <li class="slide-container">
-        //                 <div class="slide-image">
-        //                 <section className='desc__block2' id='slider'>
-        //                      <img src={GirlTable} alt="Girl" />
-        //                      <div className='desc__block2_container'>
-        //                          <h2 className='desc__title_type'>
-        //                              Пройди тестирование
-        //                          </h2>
-        //                          <p className='desc__text_type'>
-        //                              Мы поможем тебе определить несколько подходящих профессий, исходя из твоих интересов
-        //                          </p>
-        //                      </div>
-        //                      <div className='desc__block2_three desc__block2_three_three'>
-        //                          <img 
-        //                              src={PlayBtn} 
-        //                              alt="Button" 
-        //                              className='desc__button'
-        //                          />
-        //                      </div>
-        //                  </section>
-        //                 </div>
-        //                 <div class="carousel-controls">
-        //                 <label for="img-2" class="prev-slide">
-        //                     <span>&lsaquo;</span>
-        //                 </label>
-        //                 <label for="img-1" class="next-slide">
-        //                     <span>&rsaquo;</span>
-        //                 </label>
-        //                 </div>
-        //             </li>
-        //             <div class="carousel-dots">
-        //                 <label for="img-1" class="carousel-dot" id="img-dot-1"></label>
-        //                 <label for="img-2" class="carousel-dot" id="img-dot-2"></label>
-        //                 <label for="img-3" class="carousel-dot" id="img-dot-3"></label>
-        //             </div>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </>
     );
 };
 
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,29 +1,10 @@
+import React from 'react';
 import './Slider.css';
 
 export default function Slider({ children }) {
-  // document.addEventListener('DOMContentLoaded', () => {
-  //   const slider = document.getElementById("horizontal-scroller");
-  //   console.log(slider);
-  //   slider.addEventListener('wheel', function(event) {
-  //   if (event.deltaMode === event.DOM_DELTA_PIXEL) {
-  //     var modifier = 1;
-  //     // иные режимы возможны в Firefox
-  //   } else if (event.deltaMode === event.DOM_DELTA_LINE) {
-  //     modifier = parseInt(getComputedStyle(this).lineHeight);
-  //   } else if (event.deltaMode === event.DOM_DELTA_PAGE) {
-  //     modifier = this.clientHeight;
-  //   }
-  //   if (event.deltaY !== 0) {
-  //     // замена вертикальной прокрутки горизонтальной
-  //     this.scrollLeft += modifier * event.deltaY;
-  //     event.preventDefault();
-  //   }
-  // });
-  // });
 
 function scrolling () {
   const slider = document.getElementById("horizontal-scroller");
-  console.log(slider);
   slider.addEventListener('wheel', function(event) {
     if (event.deltaMode === event.DOM_DELTA_PIXEL) {
       var modifier = 1;
